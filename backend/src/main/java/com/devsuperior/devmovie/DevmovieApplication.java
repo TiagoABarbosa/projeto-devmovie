811630d607de5ed475ca8eea74da52fa64af48b6
Tiago Barbosa <tiagosoundcar@gmail.com>
2022-01-12 21:54:54 -0300

    Project Created

---
+++ backend/src/main/java/com/devsuperior/devmovie/DevmovieApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/devmovie/DevmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.devmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
