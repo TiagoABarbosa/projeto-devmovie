098b3a6061e1290a765a71aa0f7cf06c0d625676
Tiago Barbosa <tiagosoundcar@gmail.com>
2022-01-13 23:52:19 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,10 @@
+import Pagination from "components/Pagination";
+
 function Listing () {
 
     return (
-        <h1>Página de Listagem</h1>
-    )
+        <Pagination />
+    );
 }
 
 export default Listing;
