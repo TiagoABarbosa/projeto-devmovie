9e901d601b72ca9e9e6a8268534c0196b9ac31f9
Tiago Barbosa <tiagosoundcar@gmail.com>
2022-01-15 00:16:27 -0300

    Find movies

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -1,22 +1,18 @@
 import { ReactComponent as GithubIcon } from "assets/img/github.svg";
-import { ReactComponent as LogoMovie } from "assets/img/logomovie.svg";
 import "./styles.css";
 
 function Navbar() {
+
   return (
     <header>
       <nav className="container">
         <div className="devmovie-nav-content">
           <h1>DEVMovie</h1>
 
-          <div className="devmovie-title-content">
-            <LogoMovie />
-          </div>
-
           <a href="https://github.com/TiagoABarbosa">
             <div className="devmovie-contact-content">
               <GithubIcon />
-              <p className="devmovie-contact-link">/TiagoBarbosa</p>
+              <p className="devmovie-contact-link">/ tiagoBarbosa</p>
             </div>
           </a>
         </div>
