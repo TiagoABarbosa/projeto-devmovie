7331622d1bfcf08926f65ae94efb66754158c489
Tiago Barbosa <tiagosoundcar@gmail.com>
2022-01-15 17:42:05 -0300

    useState, useEffect

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,21 @@
+const Footer = () => {
+  return (
+    <footer className="text-center py-2">
+      <div className="container">
+        <p className="text-secondary">
+          Aplicativo desenvolvido por{" "}
+          <a
+            href="https://www.linkedin.com/in/tiagoalvestec/"
+            target="_blank"
+            rel="noreferrer">Tiago Barbosa
+          </a>
+        </p>
+        <p className="text-secondary">
+          <>Spring Boot React Full Stack | 2022</>
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,39 +1,51 @@
 import axios from "axios";
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import Footer from "components/Footer";
 import { BASE_URL } from "utils/requests";
+import { useEffect, useState } from "react";
+import { MoviePage } from "types/movie";
 
 function Listing() {
 
-    //forma errada
-    axios.get(`${BASE_URL}/movies?size=12&pages=0`)
-      .then(response => {
-        console.log(response.data);
-      });
+  	const [pageNumber, setPageNumber] = useState(0);
 
-  return (
-    <>
-      <Pagination />
+
+	useEffect(() => {
+		axios.get(`${BASE_URL}/movies?size=12&pages=1`)
+  		.then((response) => {
+    		const data = response.data as MoviePage;
+    		setPageNumber(data.number);
+			});
+	  	}, []);
+	
+
+
+  	return (
+    	<>	
+			<p>{pageNumber}</p>
+      		<Pagination />
 
       <div className="container">
         <div className="row">
           <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-		  <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-		  <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-		  <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
-		  <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+          <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
             <MovieCard />
           </div>
         </div>
       </div>
+      <Footer />
     </>
   );
 }
