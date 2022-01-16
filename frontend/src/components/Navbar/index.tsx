6d366f3fde0b9f7f8d30260a8237543a2c4c2b01
Tiago Barbosa <tiagosoundcar@gmail.com>
2022-01-15 22:25:21 -0300

    navBar Sticky top

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -3,14 +3,14 @@ import "./styles.css";
 
 function Navbar() {
   return (
-    <header>
+    <header className="sticky-top">
       <nav className="container">
         <div className="devmovie-nav-content">
           <a href="/">
             <h1>DEVMovie</h1>
           </a>
 
-          <a href="https://github.com/TiagoABarbosa">
+          <a href="https://github.com/TiagoABarbosa" target="_blank" rel="noreferrer">
             <div className="devmovie-contact-content">
               <GithubIcon />
               <p className="devmovie-contact-link">/ TiagoBarbosa</p>
