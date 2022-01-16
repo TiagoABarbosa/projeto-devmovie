52b7296f26427939be2273989d0ab7ead3ffdbe1
Tiago Barbosa <tiagosoundcar@gmail.com>
2022-01-15 21:17:31 -0300

    Save, useNavigate

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -7,21 +7,23 @@ type Props = {
   movie: Movie;
 };
 
-function MovieCard( { movie } : Props) {
-  
-    return (
-        <div>
-            <img className="dsmovie-movie-card-image" src={movie.image} alt={movie.title}/>
-             <div className="dsmovie-card-bottom-container">
-                <h3>{movie.title}</h3>
-                <MovieScore count={movie.count} score={movie.score} />
+function MovieCard({ movie }: Props) {
+  return (
+    <div>
+      <img
+        className="dsmovie-movie-card-image"
+        src={movie.image}
+        alt={movie.title}
+      />
+      <div className="dsmovie-card-bottom-container">
+        <h3>{movie.title}</h3>
+        <MovieScore count={movie.count} score={movie.score} />
 
-                <Link to={`/form/{$movie.id}`}>
-                    <div className="btn btn-warning dsmovie-btn">Avaliar</div>
-                </Link>
-        </div>
+        <Link to={`/form/${movie.id}`}>
+          <div className="btn btn-warning dsmovie-btn">Avaliar</div>
+        </Link>
+      </div>
     </div>
-    
   );
 }
 
