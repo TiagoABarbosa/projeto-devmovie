037fa9faab4825836552ffff7cb2a253225238f5
Tiago Barbosa <tiagosoundcar@gmail.com>
2022-01-17 00:15:18 -0300

    Footer message

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,15 +3,13 @@ const Footer = () => {
     <footer className="text-center py-2">
       <div className="container">
         <p className="text-secondary">
-          Aplicativo desenvolvido por{" "}
-          <a
-            href="https://www.linkedin.com/in/tiagoalvestec/"
-            target="_blank"
-            rel="noreferrer">Tiago Barbosa
-          </a>
+          <>Aguarde 20 segundos para iniciar o Banco de Dados</>
         </p>
         <p className="text-secondary">
-          <>Spring Boot React Full Stack | 2022</>
+          Aplicativo desenvolvido por{" "}
+          <a href="https://www.linkedin.com/in/tiagoalvestec/" target="_blank" rel="noreferrer">
+            Tiago Barbosa
+          </a>
         </p>
       </div>
     </footer>
